af51dc7f3a4cf2045f252e372709cade7521d8e5
Ben Mitchell <ben.mitchell@canva.com>
2024-12-24 10:16:37 +1100

    added hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { GameProvider } from "./context/GameContext";
 import StartingScreen from "./components/StartingScreen";
 import CardSelection from "./components/CardSelection";
-import GameRound from "./components/GameRound";
-import EndGame from "./components/EndGame";
 import ReadyScreen from "./components/ReadyScreen";
+import GameRound from "./components/GameRound";
 import RoundEndScreen from "./components/RoundEndScreen";
+import EndGame from "./components/EndGame";
 
 const App = () => {
   return (
-    <GameProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<StartingScreen />} />
-          <Route path="/card-selection" element={<CardSelection />} />
-          <Route path="/ready-screen" element={<ReadyScreen />} />
-          <Route path="/game-round" element={<GameRound />} />
-          <Route path="/round-end" element={<RoundEndScreen />} />{" "}
-          {/* Add this */}
-          <Route path="/end-game" element={<EndGame />} />
-        </Routes>
-      </Router>
-    </GameProvider>
+    <Router basename="/monikers-webapp">
+      {" "}
+      {/* Add basename here */}
+      <Routes>
+        <Route path="/" element={<StartingScreen />} />
+        <Route path="/card-selection" element={<CardSelection />} />
+        <Route path="/ready-screen" element={<ReadyScreen />} />
+        <Route path="/game-round" element={<GameRound />} />
+        <Route path="/round-end" element={<RoundEndScreen />} />
+        <Route path="/end-game" element={<EndGame />} />
+      </Routes>
+    </Router>
   );
 };
 
